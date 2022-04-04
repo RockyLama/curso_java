5312b1ca91b3efd6d6f07427322865ebaccca286
RockyLama <89877639+RockyLama@users.noreply.github.com>
2022-04-04 13:24:48 -0300

    Create raices.java

---
+++ raices.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class raices {
+
+    public static void main(String[] args) {
+        int num,disc;
+        
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese un numero");
+        num = teclado.nextInt();
+        System.out.println("Ingrese el discriminante");
+        disc = teclado.nextInt();
+        
+        System.out.println("");
+
+    }
+}
