57fcad9eb914dde3c7dd1a26c601bf5f4b864248
RockyLama <89877639+RockyLama@users.noreply.github.com>
2022-04-04 13:24:36 -0300

    Create capicua.java

---
+++ capicua.java
@@ -0,0 +1,8 @@
+public class capicua {
+    
+    static Scanner entrada = new Scanner(System.in);
+    public static void main(String[] args){
+        int valor, v1,v2,v3,v4,v5;
+        
+    }
+}
